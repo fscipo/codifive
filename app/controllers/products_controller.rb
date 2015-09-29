2fecf5cbbaa5bfcc0fca3770969d0d30dba739c5
vanesa.lorena.mamani <vanesa.lorena.mamani@accenture.com>
2015-09-29 18:25:38 -0300

    Update products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,17 @@ def index
 	@prod = Product.all
 
 end
+def new
+end
 
 
-#def create
- # @prod = (Product.where(params[:nombre]) or Product.where(params[:descripcion]))
+
+def create
+  @prod = (Product.where(params[:nombre]) or Product.where(params[:descripcion]))
+  prod.save
+  redirect_to Product_path
+
   
-#end
-
+end
 
 end
