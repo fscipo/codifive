cb2a297900a18ba0c2bc19970ff57ea430286893
Fran Santos <fransantos.cipo@gmail.com>
2015-10-06 19:01:33 -0300

    Agregue los mensajes al crear y modificar las categorías con sus respectivos errores

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
 	has_many :products
 	validates :nombre,
 		presence: true,
-		uniqueness: true
+		uniqueness: {message: "No se puede repetir las categorias"}
 
 	default_scope -> { order("nombre") }
 end
