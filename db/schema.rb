fdd6786def3f84f86062d11d5e863f4acceb8333
Fran Santos <fransantos.cipo@gmail.com>
2015-10-06 19:54:04 -0300

    Crear columna vencimiento en producto

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922214256) do
+ActiveRecord::Schema.define(version: 20151006222928) do
 
   create_table "categories", force: true do |t|
     t.string   "nombre"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150922214256) do
     t.string   "foto"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "vencimiento"
   end
 
 end
