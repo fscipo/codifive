7dab4bb81f2c6fa4fedc2c74735df78097834bae
Fran Santos <exit>
2015-11-03 19:33:45 -0300

    Cambios para subir a heroku

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,14 +2,14 @@ class OffersController < ApplicationController
 
 
 
-	def create
-		@offer= Offer.new(params.require(:offer).permit(:razon, :client_id, :precio, :tarjeta, :codigo, :client_id)
-		if @offer.save
-			flash[:success]= "Se creado la oferta correctamente"
-			redirect_to products_path
-		else
-			flash[:danger]= "Ocurrio un error al guardar la oferta"
-			render :new
-		end
-	end
+	#def create
+	#	@offer= Offer.new(params.require(:offer).permit(:razon, :client_id, :precio, :tarjeta, :codigo, :client_id)
+	#	if @offer.save
+	#		flash[:success]= "Se creado la oferta correctamente"
+	#		redirect_to products_path
+	#	else
+	#		flash[:danger]= "Ocurrio un error al guardar la oferta"
+	#		render :new
+	#	end
+#	end
 end
